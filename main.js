324ddb4dd4829d4494e038b26441b88ae654d964
Amy Butz <amymbutz@gmail.com>
2017-05-10 19:06:56 -0500

    finished the project

--- main.js
+++ main.js
@@ -3,15 +3,21 @@ console.log("Your document is ready!");
   $(".button").on("click", function(){
       if ($(this).hasClass("lie")) {
         $(".button").removeAttr("id");
-        /*$(this).css({'border-color': '#56A589', 'color' :'#56A589'});*/
         $(this).attr('id',"false");
-        $("header").text("Correct!").css({'text-align' : 'center', 'font-weight' : 'bold', 'color' : '#56A589' });
+        $("header").text("Correct!").css({'margin-left' : '0px', 'text-align' : 'center', 'font-weight' : 'bold', 'color' : '#56A589'});
+        $('#next').css({'display' : 'block'});
       } else {
         $(".button").removeAttr("id");
-        /*$(this).css({'border-color' :'#DB6f6f', 'color' :'#DB6f6f'});*/
         $(this).attr('id',"true");
-        $("header").text("Wrong, Try Again").css({'text-align' : 'center', 'font-weight' : 'bold', 'color' : '#DB6f6f' });
+        $("header").text("Wrong, Try Again").css({'margin-left' : '0px', 'text-align' : 'center', 'font-weight' : 'bold', 'color' : '#DB6f6f' });
       }
   });
+
+  $("#next").on("click", function(){
+    $('#Amy').css({'display' : 'none'});
+    $('#Aalia').css({'display' : 'block'});
+    $("header").text("Guess the Lie...about Aalia...").css({'margin-left' : '20px', 'text-align' : 'left', 'font-weight' : '400', 'color' : 'black' });
+  }
+)
 
 });
